e8eace551f74ba0744e6d51ef3fad4be4ac94955
abhirmehra5 <107518702+abhirmehra5@users.noreply.github.com>
2022-07-21 09:34:10 -0400

    Add files via upload

---
+++ ibdata.py
@@ -0,0 +1,24 @@
+from ib_insync import *
+import pandas as pd
+data = pd.read_csv("tickers.csv")
+stocks = data["Ticker"]
+df2=pd.DataFrame(columns = ["contract","industry","longName","marketName","secIdList"])
+df3=pd.DataFrame(columns = ["contract","industry","longName","marketName","secIdList"])
+
+ib = IB()
+ib.connect('127.0.0.1', 7496, clientId=99)
+for i in stocks:
+    contract = Stock(i)
+    try:
+        bars = ib.reqContractDetails(contract)
+        df = pd.DataFrame(bars)
+        cleandata = df[["contract","industry","longName", "marketName", "secIdList"]]
+        cleandata = cleandata.explode('secIdList')
+        cleandata = cleandata.drop_duplicates(subset=["secIdList"])
+        df2 = pd.concat([df2, cleandata])
+    except:
+        df2 =pd.concat([df2,df3])
+
+
+df2.to_csv('unclean_ibdata.csv')
+
